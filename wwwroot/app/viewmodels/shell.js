c17cd5a245d47915ecf9927e3324120efabb68d7
enthudev <enthudev@live.in>
2016-06-12 23:40:07 +0530

    Checking in before changing the folder structure for viewmodels and views

--- wwwroot/app/viewmodels/shell.js
+++ wwwroot/app/viewmodels/shell.js
@@ -1,5 +1,13 @@
-﻿export class shell {
+﻿import {Events} from 'viewmodels/events';
+export class shell {
     constructor() {
         this.parentprop = "Hug your parents!";
+    }
+    configureRouter(config, router) {
+        this.router = router;
+        config.title = "Capital Area .NET Users Group";
+        config.map([
+            {route:['', 'events'], moduleId:'viewmodels/events'}
+        ])
     }
 }
